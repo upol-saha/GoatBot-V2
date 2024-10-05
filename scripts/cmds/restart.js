231254052554d0cc2e4ab0171ee4740d0539888d
Mr.Upol <142928324+upol-saha@users.noreply.github.com>
2024-10-05 10:27:20 -0700

    Update restart.js

--- scripts/cmds/restart.js
+++ scripts/cmds/restart.js
@@ -1,49 +1,34 @@
 const fs = require("fs-extra");
 
 module.exports = {
-	config: {
-		name: "restart",
-		version: "1.0",
-		author: "NTKhang",
-		countDown: 5,
-		role: 2,
-		shortDescription: {
-			vi: "Khởi động lại bot",
-			en: "Restart bot"
-		},
-		longDescription: {
-			vi: "Khởi động lại bot",
-			en: "Restart bot"
-		},
-		category: "Owner",
-		guide: {
-			vi: "   {pn}: Khởi động lại bot",
-			en: "   {pn}: Restart bot"
-		}
-	},
+  config: {
+    name: "r",
+    aliases: ['restart'],
+    author: "NTKhang + UPOL",
+    role: 2,
+    category: "Owner",
+    guide: {
+      en: "   {pn}: Restart bot"
+    }
+  },
+  langs: {
+    en: {
+      restartting: "⏳ | MeGu_AI Rebooting..."
+    }
+  },
+  onLoad: function ({ api }) {
+    const pathFile = `${__dirname}/tmp/restart.txt`;
+    if (fs.existsSync(pathFile)) {
+      const [tid, time] = fs.readFileSync(pathFile, "utf-8").split(" ");
+      api.sendMessage(`✅ MeGu_AI Rebooted | ⏰Time: ${(Date.now() - time) / 1000}s`, tid);
+      fs.unlinkSync(pathFile);
+    }
+  },
 
-	langs: {
-		vi: {
-			restartting: "🔄 | Đang khởi động lại bot..."
-		},
-		en: {
-			restartting: "🔄 | Restarting bot..."
-		}
-	},
-
-	onLoad: function ({ api }) {
-		const pathFile = `${__dirname}/tmp/restart.txt`;
-		if (fs.existsSync(pathFile)) {
-			const [tid, time] = fs.readFileSync(pathFile, "utf-8").split(" ");
-			api.sendMessage(`✅ | Bot restarted\n⏰ | Time: ${(Date.now() - time) / 1000}s`, tid);
-			fs.unlinkSync(pathFile);
-		}
-	},
-
-	onStart: async function ({ message, event, getLang }) {
-		const pathFile = `${__dirname}/tmp/restart.txt`;
-		fs.writeFileSync(pathFile, `${event.threadID} ${Date.now()}`);
-		await message.reply(getLang("restartting"));
-		process.exit(2);
-	}
+  onStart: async function ({ message, event, getLang }) {
+    const pathFile = `${__dirname}/tmp/restart.txt`;
+    fs.writeFileSync(pathFile, `${event.threadID} ${Date.now()}`);
+    await message.reply(getLang("restartting"));
+    process.exit(2);
+  }
 };
