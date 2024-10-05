46b5c90fe795e051a7f13d073d757dda3b32c9c5
Mr.Upol <142928324+upol-saha@users.noreply.github.com>
2024-10-05 22:53:37 +0600

    Update balance.js

--- scripts/cmds/balance.js
+++ scripts/cmds/balance.js
@@ -1,50 +1,34 @@
+const fs = require('fs-extra');
+const { getPrefix } = global.utils;
+
 module.exports = {
-	config: {
-		name: "balance",
-		aliases: ["bal"],
-		version: "1.1",
-		author: "NTKhang",
-		countDown: 5,
-		role: 0,
-		shortDescription: {
-			vi: "xem số tiền của bạn",
-			en: "view your money"
-		},
-		longDescription: {
-			vi: "xem số tiền hiện có của bạn hoặc người được tag",
-			en: "view your money or the money of the tagged person"
-		},
-		category: "economy",
-		guide: {
-			vi: "   {pn}: xem số tiền của bạn"
-				+ "\n   {pn} <@tag>: xem số tiền của người được tag",
-			en: "   {pn}: view your money"
-				+ "\n   {pn} <@tag>: view the money of the tagged person"
-		}
-	},
-
-	langs: {
-		vi: {
-			money: "Bạn đang có %1$",
-			moneyOf: "%1 đang có %2$"
-		},
-		en: {
-			money: "You have %1$",
-			moneyOf: "%1 has %2$"
-		}
-	},
-
-	onStart: async function ({ message, usersData, event, getLang }) {
-		if (Object.keys(event.mentions).length > 0) {
-			const uids = Object.keys(event.mentions);
-			let msg = "";
-			for (const uid of uids) {
-				const userMoney = await usersData.get(uid, "money");
-				msg += getLang("moneyOf", event.mentions[uid].replace("@", ""), userMoney) + '\n';
-			}
-			return message.reply(msg);
-		}
-		const userData = await usersData.get(event.senderID);
-		message.reply(getLang("money", userData.money));
-	}
+  config: {
+    name: 'balance',
+    aliases: ['bal'],
+    version: '500',
+    role: 0,
+    author: 'UPoL The MiMis Momo ☺️🌸',
+    shortDescription: 'Check your balanace',
+    longDescription: 'Check your balance and see other user balance whit mention or reply .',
+    guide: '${pn} [empty | @tag | reply]',
+  },
+  langs: {
+    en: {
+      money: "You have %1$",
+      moneyOf: "%1 has %2$"
+    }
+  },
+    onStart: async function ({ message, usersData, event, getLang }) {
+    if (Object.keys(event.mentions).length > 0) {
+      const uids = Object.keys(event.mentions);
+      let msg = "";
+      for (const uid of uids) {
+        const userMoney = await usersData.get(uid, "money");
+        msg += getLang("moneyOf", event.mentions[uid].replace("@", ""), userMoney) + '\n';
+      }
+      return message.reply(msg);
+    }
+    const userData = await usersData.get(event.senderID);
+    message.reply(getLang("money", userData.money));
+    }
 };
